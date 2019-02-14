bb525060906af7bf280b3b8ebd01a570c75963b4
Peter Perez <peter.perez@outlook.com>
2019-02-14 09:28:40 -0700

    navbar mostly complete, needs css fine tuning

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,80 +1,27 @@
 import React, { Component } from 'react';
-import imgSrc from '../../asset/MSUD_Informal_2CPos_125x.png';
-import Dropdown from '../dropdownmenu/Dropdown';
-import '../../asset/sharedstyles.css';
-//import '../Search.search.js';
-
-class NavBar extends Component {
-    render() {
-        return (
-            // <div align="left">
-            //     {/* <a href="#">
-            //         <img src={imgSrc} style={imgStyle} home />
-
-            //     </a> */}
-            <div className='navbar' style={navStyle} align="left">
+//import logo from './logo.svg';
+import './App.css';
+import './components/nav/NavBar';
+import NavBar from './components/nav/NavBar';
 
 
-                <ul style={listStyle}>
-                    <li style={leftLi}><img src={imgSrc} style={imgStyle} /></li>
-                    <li style={leftLi}><a href='#home' style={linkStyle} >Home</a></li>
-                    <li style={leftLi}><a href='#www.1.com' style={linkStyle} >My Activities</a></li>
-                    <form>
 
-                    </form>
-                    {/* <li style={rightLi}><a href='#www.3.com' style={linkStyle} >Link3</a></li> */}
-                    <li style={drpDwnLi}>
-                        <Dropdown /></li>
-                </ul>
 
-            </div>
-            // </div>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <NavBar />
+        </div>
+        <header className="App-header">
+
+
+
+        </header>
+      </div>
+    );
+  }
 }
 
-const listStyle = {
-    listStyleType: 'none',
-    margin: 0,
-    padding: 0,
-    overflow: 'hidden',
-    backgroundColor: '#000000',
-    width: '100%'
-}
-
-const navStyle = {
-    width: '100%',
-    position: 'fixed',
-    top: '0'
-}
-
-const linkStyle = {
-    display: 'block',
-    backgroundColor: '#000000',
-    color: 'white',
-    textAlign: 'center',
-    padding: '14px 16px',
-    textDecoration: 'none',
-}
-
-const leftLi = {
-    float: 'left'
-}
-
-const rightLi = {
-    float: 'right'
-}
-const cntrLi = {
-    float: 'center'
-}
-
-const imgStyle = {
-    height: '50px',
-    width: '50px'
-}
-
-const drpDwnLi = {
-    display: 'flex',
-    float: 'right'
-}
-export default NavBar;
+export default App;
