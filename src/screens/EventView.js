d605ccac507d7cd86ffcbc2b8816035f41a9a785
Peter Perez <peter.perez@outlook.com>
2019-02-21 13:09:23 -0700

    test

--- src/screens/EventView.js
+++ src/screens/EventView.js
@@ -7,13 +7,13 @@ class EventView extends Component {
       return (
          <div >
             <ul style={{ listStyleType: 'none', }}>
-               <li style={{width:'80px', display:'inline', margin:'0 auto', padding:'0'}}>
-            <div style={{margin: '0 auto', width: '100%', height: '100%',}}>
+               <li style={{width:'30%px', display:'inline', margin:'0 auto', padding:'0'}}>
+            <div style={{margin: '0 auto',}}>
                <img src={map} style={{ width: '80vh', height: '60vh', border: '2px solid #000000', float: 'left' }} />
             </div>
             </li>
             
-            <li style={{width:'80px',  display:'inline', margin:'0 auto', padding:'0'}}>
+            <li style={{width:'30%',  display:'inline', margin:'0 auto', padding:'0'}}>
             <div style={{ float: 'right' }}>
                <ul>
                   <li>  <text>Event Name: Mighty Fine Shindig</text>  </li>
@@ -24,8 +24,8 @@ class EventView extends Component {
             </div>
             </li>
 
-            <li style={{width:'80px',  display:'inline', margin:'0 auto', padding:'0'}}>
-            <div style={{margin: '0 auto', height:'80vh', width:'60vh'}}>
+            <li style={{width:'30%',  display:'inline', margin:'0 auto', padding:'0'}}>
+            <div style={{margin: '0 auto', }}>
                <img src={addpicture} style={{ width: '80vh', height: '60vh', border: '2px solid #000000', float: 'left' }} />
             </div>
             </li>
