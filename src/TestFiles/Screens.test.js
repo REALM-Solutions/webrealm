73d903679d6d50ee3cc3374647eef8758bffc6e3
Peter Perez <peter.perez@outlook.com>
2019-02-26 16:45:46 -0700

    added snapshot tests to pages

---
+++ src/TestFiles/Screens.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import HomePage from '../screens/HomePage';
+import EventView from '../screens/EventView';
+import CreateEventPage from '../screens/CreateEventPage';
+import renderer from 'react-test-renderer';
+
+test('Homepage renders correctly', () => {
+   const tree = renderer.create(<HomePage />).toJSON();
+   expect(tree).toMatchSnapshot();
+})  
+
+test('EventView renders correctly', () => {
+   const tree = renderer.create(<EventView />).toJSON();
+   expect(tree).toMatchSnapshot();
+})  
+
+test('CreateEventPage renders correctly', () => {
+   const tree = renderer.create(<CreateEventPage />).toJSON();
+   expect(tree).toMatchSnapshot();
+})  
