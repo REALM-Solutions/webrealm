f9822e68d973492c1f69ab7da56032f5a9ac4d5d
Peter Perez <peter.perez@outlook.com>
2019-03-04 20:13:27 -0700

    added validation but this broke the submit

--- src/screens/CreateEventPage.js
+++ src/screens/CreateEventPage.js
@@ -14,48 +14,54 @@ class CreateEventPage extends Component {
          eventStartTimeText: '',
          eventEndTimeText: '',
       }
-      
+
+      this.form = React.createRef();
+      this.validate = this.validate.bind(this);
+
       this.handleSubmit = this.handleSubmit.bind(this)
+   }
+   validate() {
+      this.form.current.reportValidity();
    }
 
 
    handleSubmit(event) {
       if (this.state.category === '' || this.state.eventNameText === '' || this.state.eventLocationText === ''
-           || this.state.eventDateText === '' || this.state.eventStartTimeText === '' || this.state.eventEndTimeText === '') {
-               window.alert("All fields must be filled in")
-               console.log(this.state)
-           }
-           console.log(this.state)
-           window.alert("Event: " + this.state.eventNameText + " successfully created!")
-           this.setState({
-            category: '',
-            eventNameText: '',
-            eventDescriptionText: '',
-            eventLocationText: '',
-            eventDateText: '',
-            eventStartTimeText: '',
-            eventEndTimeText: '',
-           })
+         || this.state.eventDateText === '' || this.state.eventStartTimeText === '' || this.state.eventEndTimeText === '') {
+         window.alert("All fields must be filled in")
+         console.log(this.state)
+      }
+      console.log(this.state)
+      window.alert("Event: " + this.state.eventNameText + " successfully created!")
+      this.setState({
+         category: '',
+         eventNameText: '',
+         eventDescriptionText: '',
+         eventLocationText: '',
+         eventDateText: '',
+         eventStartTimeText: '',
+         eventEndTimeText: '',
+      })
    }
-   render(){
-      return(
+   render() {
+      return (
          <div>
             <div style={styles.createEventMapContainer}>
-               <img src={map} style={styles.createEventMap}/>
+               <img src={map} style={styles.createEventMap} />
             </div>
             <div style={styles.createEventContainer}>
-               <form onSubmit={this.handleSubmit} style={styles.createEventForm}>
+               <form ref={this.form} onSubmit={e => e.preventDefault()} style={styles.createEventForm}>
                   <div>
-                     <label for='eventDate'>Date</label>
-                     <input type='date' id='eventDate' onChange={(event) => this.setState({eventDateText: event.target.value})}/>
+                     <label for='eventDate' >Date</label>
+                     <input required type='date' id='eventDate' onChange={(event) => this.setState({ eventDateText: event.target.value })} />
 
                      <label for='eventStartTime'>Start Time</label>
-                     <input type='time' id='eventStartTime' onChange={(event) => this.setState({eventStartTimeText: event.target.value})}/>
+                     <input required type='time' id='eventStartTime' onChange={(event) => this.setState({ eventStartTimeText: event.target.value })} />
 
                      <label for='eventEndTime'>End Time</label>
-                     <input type='time' id='eventEndTime' onChange={(event) => this.setState({eventEndTimeText: event.target.value})}/>
+                     <input required type='time' id='eventEndTime' onChange={(event) => this.setState({ eventEndTimeText: event.target.value })} />
 
-                     <select onChange={(value) => this.setState({category: value})}>
+                     <select required onChange={(value) => this.setState({ category: value })}>
                         <option value=''>Select a Category</option>
                         <option value='sports'>Sports</option>
                         <option value='games'>Games</option>
@@ -66,13 +72,13 @@ class CreateEventPage extends Component {
                      </select>
                   </div>
                   <label for='eventName'>Event Name</label>
-                  <input type='text' id='eventName' style={styles.createEventTextInput} placeholder='Event Name' onChange={(text) => this.setState({eventNameText: text.target.value})} />
+                  <input required type='text' id='eventName' style={styles.createEventTextInput} placeholder='Event Name' onChange={(text) => this.setState({ eventNameText: text.target.value })} />
 
                   <label for='eventLocation'>Location</label>
-                  <input type='text' id='eventLocation' style={styles.createEventTextInput} placeholder='Event Location' onChange={(text) => this.setState({eventLocationText: text.target.value})} />
+                  <input required type='text' id='eventLocation' style={styles.createEventTextInput} placeholder='Event Location' onChange={(text) => this.setState({ eventLocationText: text.target.value })} />
 
                   <label for='eventDescription'>Description</label>
-                  <textarea type='text' id='eventDescription' style={styles.createEventTextInput} placeholder='Event Description' onChange={(text) => this.setState({eventDescriptionText: text.target.value})}>
+                  <textarea required type='text' id='eventDescription' style={styles.createEventTextInput} placeholder='Event Description' onChange={(text) => this.setState({ eventDescriptionText: text.target.value })}>
                   </textarea>
                   <input type='submit' value='Create Event' />
                </form>
@@ -114,8 +120,8 @@ const styles = {
       borderRadius: '4px',
       boxSizing: 'border-box',
       textAlign: 'center'
-    }
-    
+   }
+
    //  input[type=submit] {
    //    width: 100%;
    //    background-color: #4CAF50;
@@ -126,10 +132,10 @@ const styles = {
    //    border-radius: 4px;
    //    cursor: pointer;
    //  }
-    
+
    //  input[type=submit]:hover {
    //    background-color: #45a049;
    //  }
-    
+
 }
 export default CreateEventPage;
