5902e2cd155ff703ebc2736b5880d17eb559a3aa
Peter Perez <peter.perez@outlook.com>
2019-04-08 19:28:09 -0600

    fixed a few more

--- src/screens/CreateEventPage.js
+++ src/screens/CreateEventPage.js
@@ -10,8 +10,8 @@ class CreateEventPage extends Component {
       this.state = {
          fields: {},
          errors: {},
-         formIsValid: false
-
+         formIsValid: true,
+         
       }
 
       this.baseState = this.state
@@ -27,20 +27,20 @@ class CreateEventPage extends Component {
 
       //Name
       if (!fields["eventNameText"]) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventNameText"] = "Please enter an event name";
       }
 
       if (typeof fields["eventNameText"] !== "undefined") {
          if (!fields["eventNameText"].match(/^[\S\s]{3,15}$/)) {
-            this.state.formIsValid = false;
+            this.state.formIsValid=false;
             errors["eventNameText"] = "Name must be longer than 3 characters";
          }
       }
 
       //Event Date
       if (!fields["eventDate"]) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventDate"] = "Please enter a valid date";
       }
       var dateObj = new Date();
@@ -72,34 +72,34 @@ class CreateEventPage extends Component {
       console.log(dif + " " + chsnDate + " " + newDate)
 
       if (dif < 0) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventDate"] = "Please enter a valid date";
       }
 
       //Event Start Time
       if (!fields["eventStartTime"]) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventStartTime"] = "Please enter a valid time";
       }
 
       //Event End Time
       if (!fields["eventEndTime"]) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventEndTime"] = "Please enter a valid time";
       }
       if (this.state.fields.eventStartTime > this.state.fields.eventEndTime) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventEndTime"] = "This must be later than the start time."
       }
 
       //Location
       if (!fields["eventLocation"]) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventLocation"] = "Cannot be empty";
       }
       if (typeof fields["eventLocation"] !== "undefined") {
          if (!fields["eventLocation"].match(/^[\S\s]{3,12}$/)) {
-            this.state.formIsValid = false;
+            this.state.formIsValid=false;
             errors["eventLocation"] = "Location must be longer than 3 characters";
          }
       }
@@ -107,13 +107,13 @@ class CreateEventPage extends Component {
       //Description
       if (typeof fields["eventDescription"] !== "undefined") {
          if (!fields["eventDescription"].match(/^[\S\s]{5,325}$/)) {
-            this.state.formIsValid = false;
+            this.state.formIsValid=false;
             errors["eventDescription"] = "Description must be longer than 5 characters and less than 325 characters";
          }
       }
 
       if (!fields["eventDescription"]) {
-         this.state.formIsValid = false;
+         this.state.formIsValid=false;
          errors["eventDescription"] = "Please enter an event description.";
       }
 
@@ -121,6 +121,8 @@ class CreateEventPage extends Component {
       this.setState({ errors: errors });
       return this.state.formIsValid;
    }
+
+   
 
    eventSubmit(e) {
       e.preventDefault();
@@ -165,7 +167,7 @@ class CreateEventPage extends Component {
 
       } else {
          this.resetFields()
-         this.state.formIsValid = true;
+         this.state.formIsValid=true;
          console.log("something went wrong, check validation errors")
       }
 
@@ -176,18 +178,17 @@ class CreateEventPage extends Component {
       let fields = this.state.fields;
       fields[field] = e.target.value;
       this.setState({ fields });
-      this.state.formIsValid = true;
+      this.state.formIsValid=true;
       console.log(this.state.formIsValid)
    }
 
    resetFields() {
-      this.state=this.baseState
+      this.state = this.baseState
    }
 
 
    render() {
       return (
-
          <div className="createEventFormContainer" >
             <div className="createEventMapContainer" >
                <img className="createEventViewMapStyling" alt="" src={map} />
