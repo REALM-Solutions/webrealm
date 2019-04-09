cc6a52e1db2ae58a36c71ff2b1eedd11130cb54e
Peter Perez <peter.perez@outlook.com>
2019-04-09 12:35:48 -0600

    terms and conditions now function, link is from the google doc where we can edit them if necessary

--- src/screens/TermsandConditions.js
+++ src/screens/TermsandConditions.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class TermsandConditions extends Component {
    render() {
       return (
-         <p>THE TERMS AND CONDITIONS WILL GO IN HERE!!</p>
+         <iframe style={{width:'100%'}} src="https://docs.google.com/document/d/e/2PACX-1vRECMYNKNOcWY47A9Jn8X6MkPFDQsVzI29n8-RIk-Sh-Rijh1qXovIsHRXyG3IonvvF1lmkx543BdIb/pub?embedded=true"></iframe>
 
       );
    }
